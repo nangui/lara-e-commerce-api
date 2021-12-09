34a6c497b9b2a061be3c89f7ee2411dd561a451c
Adonai Elohim <adonainangui03@gmail.com>
2021-12-09 11:23:31 +0000

    Create some routes for user update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdatePasswordRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserCollection;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Auth;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -49,5 +52,29 @@ class UserController extends Controller
         if ($user) $user->delete();
 
         return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+    public function user(): ?User
+    {
+        return Auth::user();
+    }
+
+    public function updateInfo(Request $request)
+    {
+        $user = Auth::user();
+
+        $user->update($request->only('first_name', 'last_name', 'email'));
+
+        return response(UserResource::make($user), Response::HTTP_ACCEPTED);
+    }
+
+    public function updatePassword(UpdatePasswordRequest $request)
+    {
+        $user = Auth::user();
+
+        $user->update([
+            'password' => Hash::make($request->get('new_password'))
+        ]);
+        return response(UserResource::make($user), Response::HTTP_ACCEPTED);
     }
 }
---
+++ app/Http/Requests/UpdatePasswordRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'password' => 'current_password:api',
+            'new_password' => 'required|confirmed|max:255'
+        ];
+    }
+}
